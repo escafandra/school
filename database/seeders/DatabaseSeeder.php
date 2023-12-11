6f856f9310833e44803b53224d1e2c1ae6dc0011
joselomanuelo <joselomanuelo@gmail.com>
2023-12-11 16:51:25 -0500

    refactor: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,35 +2,24 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Course;
-use App\Models\Lesson;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         User::factory()
             ->admin()
             ->create([
                 'name' => 'Admin',
-                'email' => 'admin@admin.com',
+                'email' => 'admin@escafandra.tech',
             ]);
 
         User::factory()
             ->create([
                 'name' => 'Student',
-                'email' => 'student@admin.com',
+                'email' => 'student@escafandra.tech',
             ]);
-
-        Course::factory(25)
-            ->published()
-            ->has(Lesson::factory(25)->published())
-            ->create();
     }
 }
